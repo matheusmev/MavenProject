204a559ec115b6f0609018afb2ad4020e4f116c6
Matheus Ev <mmarcelinoev@gmail.com>
2017-06-07 21:36:43 -0300

    Funcionando 2

--- src/main/java/aularoger.java
+++ src/main/java/aularoger.java
@@ -7,6 +7,7 @@ public class aularoger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Funcionando 1");
+		System.out.println("Funcionando 2");
 	}
 
 }
